c1bee27992ccd74ce2d2500896c68b5085ae41e5
Amara-Balakrishna1 <abkrish14331@gmail.com>
2017-11-02 17:27:04 +0530

    basic component

---
+++ travelplan.frontend/src/TravelPlan.js
@@ -0,0 +1,13 @@
+import React, {
+  Component
+} from 'react';
+
+export default class TravelPlan extends Component {
+  render() {
+    return (
+      <div name="redx">
+        Hi Hari ? asdf
+      </div>
+    );
+  }
+}
--- travelplan.frontend/src/index.js
+++ travelplan.frontend/src/index.js
@@ -1,1 +1,10 @@
-hi
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TravelPlan from './TravelPlan';
+
+const app = document.getElementById('app-root');
+
+ReactDOM.render(
+  <TravelPlan />,
+  app
+);
