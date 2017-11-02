d144c3eb3fdc8d66f0e145e88b12850f5f4039a3
BALAKRISHNA <abkrish14331@gmail.com>
2017-11-03 00:46:45 +0530

    form

---
+++ travelplan.frontend/src/components/UserForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Control, Form } from 'react-redux-form';
+
+class UserForm extends Component {
+  handleSubmit(user) {
+    console.log(user);
+    // Do whatever you like in here.
+    // If you connect the UserForm to the Redux store,
+    // you can dispatch actions such as:
+    // dispatch(actions.submit('user', somePromise));
+    // etc.
+  }
+  render() {
+    return (
+      <Form
+        model="user"
+        onSubmit={(user) => this.handleSubmit(user)}
+      >
+        <label htmlFor="user.firstName">First name</label>
+        <Control.text model="user.firstName" id="user.firstName" />
+
+        <label htmlFor="user.lastName">Last name:</label>
+        <Control.text model="user.lastName" id="user.lastName" />
+
+        <button type="submit">
+          Finish registration!
+        </button>
+      </Form>
+    );
+  }
+}
+
+export default UserForm;
